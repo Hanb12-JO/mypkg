ebb17765978d80aba204f4be098a9f571b4a1f24
Abelrahman Alhanbali <abdelrahman.alhanbali@gmail.com>
2024-12-05 12:39:30 +0900

    Add Int16

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,22 +1,22 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from std_msgs.msg import Int16
 
-rclpy.init()
-node = Node("talker")
-pub = node.create_publisher(Person, "countup", 10)
-n = 20
+class Talker(Node):
+    def __init__(self):
+        super().__init__("Talker")
+        self.pub = self.create_publisher(Int16, "countup", 10)
+        self.create_timer(0.5, self.cb)
+        self.n = 0
 
-
-def cb():
-    global n
-    msg = Person()
-    msg.name = "Abdelrahman Alhanbali"
-    msg.age = n
-    pub.publish(msg)
-    n += 1
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
 
 
 def main():
-    node.create_timer(0.5, cb)
+    rclpy.init()
+    node = Talker()
     rclpy.spin(node)
