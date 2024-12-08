043842ba9b988dfdc8ff9b047d67de3a4743b8ef
Abelrahman Alhanbali <abdelrahman.alhanbali@gmail.com>
2024-12-08 23:19:21 +0900

    Apdated

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -5,15 +5,15 @@ import launch_ros.actions
 
 
 def generate_launch_description():
-
     talker = launch_ros.actions.Node(
-        package='mypkg',      #パッケージの名前を指定
-        executable='talker',  #実行するファイルの指定
-        )
-    listener = launch_ros.actions.Node(
-        package='mypkg',
-        executable='listener',
-        output='screen'        #ログを端末に出すための設定
-        )
+            package = 'mypkg',
+            executable = 'talker',
+            )
 
-    return launch.LaunchDescription([talker, listener]) 
+    listener = launch_ros.actions.Node(
+            package = 'mypkg',
+            executable = 'listener',
+            output = 'screen'
+            )
+
+    return launch.LaunchDescription([talker, listener])
