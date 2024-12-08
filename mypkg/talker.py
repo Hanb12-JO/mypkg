2b119ffc7fd78a2eb7b601e14bd1810dc69e5e5c
Abelrahman Alhanbali <abdelrahman.alhanbali@gmail.com>
2024-12-09 00:57:17 +0900

    Apdate with using class

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -2,12 +2,14 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
+
 class Talker(Node):
     def __init__(self):
-        super().__init__("Talker")
+        super().__init__("takler")
         self.pub = self.create_publisher(Int16, "countup", 10)
         self.create_timer(0.5, self.cb)
         self.n = 0
+
 
     def cb(self):
         msg = Int16()
